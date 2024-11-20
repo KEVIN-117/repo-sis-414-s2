8badb024e789a8e0460ab2bf88fc8a7dce1845ea
k3v1n <krodriguezcondori34@gmai.com>
2024-11-20 15:17:22 -0400

    CRUD api post

--- examples/server/app/Http/Controllers/category/CategoryController.php
+++ examples/server/app/Http/Controllers/category/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\category;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -14,7 +15,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        $categories = Category::all();
+        $categories = Category::all(); // recupera todas las categorias
 
         return response()->json([
             'categories' => $categories,
@@ -28,6 +29,26 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         //
+        $validate = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]); // validación de los campos
+
+        if ($validate->fails()) {
+            return response()->json([
+                'errors' => $validate->errors(),
+                'status' => 400,
+                'message' => 'Validation Error'
+            ]); // si falla la validación
+        }
+
+        $category = Category::create($request->all()); // crea la categoría
+
+        return response()->json([
+            'category' => $category,
+            'status' => 201,
+            'message' => 'Category created successfully'
+        ]); // si se crea la categoría responde con la categoría creada
     }
 
     /**
@@ -36,6 +57,20 @@ class CategoryController extends Controller
     public function show(string $id)
     {
         //
+        $category = Category::find($id); // busca la categoría por id
+
+        if (!$category) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found'
+            ]);
+        } // verifica si la categoría no existe, respondiendo con un mensaje de error
+
+        return response()->json([
+            'category' => $category,
+            'status' => 200,
+            'message' => 'Category found'
+        ]); // si la categoría existe, responde con la categoría encontrada
     }
 
     /**
@@ -44,6 +79,22 @@ class CategoryController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $category = Category::find($id); // busca la categoría por id
+
+        if (!$category) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found'
+            ]);
+        } // verfiica si la categoría no existe, respondiendo con un mensaje de error
+
+        $category->update($request->all()); // actualiza la categoría
+
+        return response()->json([
+            'category' => $category,
+            'status' => 200,
+            'message' => 'Category updated successfully'
+        ]); // respondiendo con la categoría actualizada
     }
 
     /**
@@ -52,5 +103,20 @@ class CategoryController extends Controller
     public function destroy(string $id)
     {
         //
+        $category = Category::find($id); // busca la categoría por id
+
+        if (!$category) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Category not found'
+            ]);
+        } // verifica si la categoría no existe, respondiendo con un mensaje de error
+
+        $category->delete(); // elimina la categoría
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category deleted successfully'
+        ]); // responde con un mensaje de éxito
     }
 }
--- examples/server/app/Http/Controllers/post/PostController.php
+++ examples/server/app/Http/Controllers/post/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\post;
 use App\Http\Controllers\Controller;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -15,6 +16,13 @@ class PostController extends Controller
     {
         //
         $posts = Post::all();
+
+        if (!$posts) {
+            return response()->json([
+                'message' => 'No posts found',
+                'status' => 404,
+            ]);
+        }
 
         return response()->json([
             'posts' => $posts,
@@ -28,6 +36,62 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+        $request["slug"] = $this->createSlug('CI/CD DevOps');
+
+        $validate = Validator::make($request->all(), [
+            'user_id' => 'required|string',
+            'title' => 'required|string',
+            'content' => 'required',
+            'summary' => 'required|string',
+            'published_at' => 'required|string',
+            'category_id' => 'required|string'
+        ]);
+
+        /*
+        request -> [
+            'user_id' => '1',
+            'title' => 'CI/CD DevOps',
+            'content' => 'CI/CD DevOps es una metodología de desarrollo de software que se basa en la integración continua, la entrega continua y la implementación continua.',
+            'summary' => 'CI/CD DevOps es una metodología de desarrollo de software que se basa en la integración continua, la entrega continua y la implementación continua.',
+            'published_at' => '2024-11-13 18:45:53',
+            'category_id' => '1'
+        ]
+        */
+
+        if ($validate->fails()) {
+            return response()->json([
+                'errors' => $validate->errors(),
+                'status' => 400,
+                'message' => 'Validation Error'
+            ]);
+        }
+
+        /*
+        request -> [
+            'user_id' => '1',
+            'title' => 'CI/CD DevOps',
+            'content' => 'CI/CD DevOps es una metodología de desarrollo de software que se basa en la integración continua, la entrega continua y la implementación continua.',
+            'summary' => 'CI/CD DevOps es una metodología de desarrollo de software que se basa en la integración continua, la entrega continua y la implementación continua.',
+            'published_at' => '2024-11-13 18:45:53',
+            'category_id' => '1',
+            'slug' => 'ci-cd-devops'
+        ]
+        */
+
+        $post = Post::create($request->all());
+
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not created',
+                'status' => 500,
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Post created successfully',
+            'post' => $post->id,
+            'status' => 201,
+        ]);
     }
 
     /**
@@ -36,6 +100,20 @@ class PostController extends Controller
     public function show(string $id)
     {
         //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not created',
+                'status' => 404,
+            ]);
+        }
+
+        return response()->json([
+            'post' => $post,
+            'status' => 200,
+            'message' => 'Post found'
+        ]);
     }
 
     /**
@@ -44,6 +122,22 @@ class PostController extends Controller
     public function update(Request $request, string $id)
     {
         //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not created',
+                'status' => 404,
+            ]);
+        }
+
+        $post->update($request->all());
+
+        return response()->json([
+            'message' => 'Post updated successfully',
+            'post' => $post->id,
+            'status' => 201,
+        ]);
     }
 
     /**
@@ -52,5 +146,29 @@ class PostController extends Controller
     public function destroy(string $id)
     {
         //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json([
+                'message' => 'Post not created',
+                'status' => 404,
+            ]);
+        }
+
+        $post->delete();
+
+        return response()->json([
+            'message' => 'Post deleted successfully',
+            'status' => 200,
+        ]);
+    }
+
+    private function createSlug(string $name): string
+    {
+        $name = strtolower($name); // CI/CD DevOps -> ci/cd devops
+        $name = preg_replace('/[^a-z0-9]+/', '-', $name); // reemplaza los caracteres especiales por guiones -> ci-cd-devops
+        $name = trim($name, "-"); // elimina los guiones al principio y al final -> -ci-cd-devops- -> ci-cd-devops
+        $name = preg_replace('/_+/', '-', $name);  // ci__cd devops -> ci-cd devops
+        return $name; // ci-cd-devops
     }
 }
